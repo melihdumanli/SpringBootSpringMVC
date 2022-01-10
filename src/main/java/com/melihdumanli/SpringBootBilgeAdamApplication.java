e93538f209ae4a31c48f270b20a3ba396c5a2075
Melih DUMANLI <melihdumanli@hotmail.com>
2022-01-10 09:52:58 +0300

    Initial commit

---
+++ src/main/java/com/melihdumanli/SpringBootBilgeAdamApplication.java
@@ -0,0 +1,13 @@
+package com.melihdumanli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBilgeAdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBilgeAdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melihdumanli/SpringBootBilgeAdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melihdumanli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBilgeAdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
